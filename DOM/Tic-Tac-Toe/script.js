3da80aaa9ef61da0b0f5e0fbbd09bca6d8191a72
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-11-24 18:18:00 +0530

    tic tac toe completed

--- DOM/Tic-Tac-Toe/script.js
+++ DOM/Tic-Tac-Toe/script.js
@@ -3,9 +3,19 @@ let gameStm = document.querySelector(".gameStatement");
 let gameBoard = document.querySelector(".board");
 let allCells = document.querySelectorAll(".cell");
 
+
+let startSound = new Audio("./gamestart.mp3");
+let tieSound = new Audio("./tie.mp3");
+let victoryAudio = new Audio("./vicAud.mp3");
+let clickSound = new Audio("./click12.aac");
+let wrongClick = new Audio("./wrongclickTrimmed.wav");
+
 let gamePlay = false;
 let turn = "X";
-let count = 0 ;
+let count = 0;
+
+let xwins = 0 ;
+let owins = 0 ;
 
 // for(let i=0;i<cell.length;i++){
 
@@ -26,54 +36,51 @@ strBtn.addEventListener("click", function () {
     gameStm.innerHTML = "";
   } else {
     gamePlay = true;
-    count = 0 ;
+    startSound.play();
+    count = 0;
     strBtn.innerHTML = " Press to restart ";
     strBtn.style.backgroundColor = "red";
     gameStm.innerHTML = "X's Turn ";
   }
+
+  console.log(xwins , owins);
 });
 gameBoard.addEventListener("click", function (event) {
   let targetEle = event.target;
-  if (gamePlay == true) {
+  if (gamePlay == true && targetEle.innerHTML=="") {
+    clickSound.play();
     targetEle.innerHTML = turn;
-     count++;
-    
+    count++;
+
     if (turn == "X") {
       turn = "0";
     } else {
       turn = "X";
     }
-    
-    gameStm.innerHTML = turn + "'s  turn ";
 
-    if(count == 9 ){
-        gameStm.innerHTML = "TIE";
-        strBtn.innerHTML = "Game is Starting ";
-        setTimeout(()=>{
-            strBtn.click();
-        } , 3000)
-        
-    }
+    gameStm.innerHTML = turn + "'s  turn ";
 
     let winRes = checkWinner();
 
     if (winRes == 1) {
       // zero is winner
       gameStm.innerHTML = "0 Wins ";
-      strBtn.innerHTML = "Game is Starting ";
-      setTimeout(()=>{
-        strBtn.click();
-    } , 3000)
-    
+      owins++;
+      victoryAudio.play();
+      restartTheGame();
     } else if (winRes == 2) {
       // x is winner
       gameStm.innerHTML = "X Wins ";
-      strBtn.innerHTML = "Game is Starting ";
-      setTimeout(()=>{
-        strBtn.click();
-    } , 3000)
-    
+      xwins++;
+      victoryAudio.play();
+      restartTheGame();
+    } else if (count == 9) {
+      gameStm.innerHTML = "TIE";
+      tieSound.play();
+      restartTheGame();
     }
+  }else{
+    wrongClick.play();
   }
 });
 
@@ -144,4 +151,13 @@ function clearAll() {
   for (let i = 0; i < allCells.length; i++) {
     allCells[i].innerHTML = "";
   }
+}
+
+function restartTheGame() {
+  strBtn.innerHTML = "Game is Starting ";
+  strBtn.disabled = true;
+  setTimeout(() => {
+    strBtn.disabled = false;
+    strBtn.click();
+  }, 3000);
 }
